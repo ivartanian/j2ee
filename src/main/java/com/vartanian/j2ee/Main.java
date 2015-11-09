ccd782a832fd4c2d8bca0eea66136c89eca322a0
super <igor.vartanian@gmail.com>
2015-11-09 17:59:11 -0500

    Book service

--- src/main/java/com/vartanian/j2ee/Main.java
+++ src/main/java/com/vartanian/j2ee/Main.java
@@ -5,16 +5,40 @@ import com.vartanian.j2ee.services.BookService;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 /**
  * Created by super on 11/6/15.
  */
 public class Main {
     public static void main(String[] args) {
-        Weld weld = new Weld();
-        WeldContainer container = weld.initialize();
-        BookService bookService = container.instance().select(BookService.class).get();
-        Book book = bookService.createBook("H2G2", 12.5f, "Интересная книга на тему высоких технологий");
-        System.out.println(book);
-        weld.shutdown();
+
+        //---------------Chapter 02---------------
+
+//        Weld weld = new Weld();
+//        WeldContainer container = weld.initialize();
+//        BookService bookService = container.instance().select(BookService.class).get();
+//        Book book = bookService.createBook("H2G2", 12.5f, "Интересная книга на тему высоких технологий");
+//        System.out.println(book);
+//        weld.shutdown();
+
+        //---------------Chapter 04---------------
+
+        // Создает экземпляр Book
+        Book book = new Book("H2G2", "Автостопом по Галактике", 12.5F, "1-84023-742-2", 354, false);
+        // Получает EntityManager и транзакцию
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("chapter04PU");
+        EntityManager em = emf.createEntityManager();
+        // Обеспечивает постоянство Book в базе данных
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(book);
+        tx.commit();
+        // Закрывает EntityManager и EntityManagerFactory
+        em.close();
+        emf.close();
     }
 }
--- src/main/java/com/vartanian/j2ee/models/Book.java
+++ src/main/java/com/vartanian/j2ee/models/Book.java
@@ -3,11 +3,33 @@ package com.vartanian.j2ee.models;
 /**
  * Created by super on 11/6/15.
  */
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "findAllBooks", query = "SELECT b FROM Book b"),
+        @NamedQuery(name = "findBookH2G2", query = "SELECT b FROM Book b WHERE b.title ='H2G2'")
+                })
 public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
     private String title;
     private Float price;
+
+    @Size(min = 10, max = 2000)
     private String description;
-    private String number;
+    private transient String number;
+
+    private String isbn;
+    private Integer nbOfPage;
+    private Boolean illustrations;
 
     public Book() {
     }
@@ -16,6 +38,23 @@ public class Book {
         this.title = title;
         this.price = price;
         this.description = description;
+    }
+
+    public Book(String title, String description, Float price, String isbn, Integer nbOfPage, Boolean illustrations) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.isbn = isbn;
+        this.nbOfPage = nbOfPage;
+        this.illustrations = illustrations;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -48,5 +87,29 @@ public class Book {
 
     public void setNumber(String number) {
         this.number = number;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getNbOfPage() {
+        return nbOfPage;
+    }
+
+    public void setNbOfPage(Integer nbOfPage) {
+        this.nbOfPage = nbOfPage;
+    }
+
+    public Boolean getIllustrations() {
+        return illustrations;
+    }
+
+    public void setIllustrations(Boolean illustrations) {
+        this.illustrations = illustrations;
     }
 }
